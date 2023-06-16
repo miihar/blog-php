834d39f391493385ee9c921ef28e814c98929a50
miihar <mbae.am1@gmail.com>
2023-06-16 09:00:06 +0400

    ajout des defferent fichier .php et autre dossier

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <p>
+        Pour plus d’actualité rejoingnez nous
+        à travers nos reseaux sociaux
+    </p>
+    <a href="https://www.whatsapp.com/"><i class="fa-brands fa-whatsapp fa-xl"></i></a>
+    <a href="https://www.facebook.com/"><i class="fa-brands fa-facebook-f fa-xl"></i></a>
+    <a href="https://www.messenger.com/"><i class="fa-brands fa-facebook-messenger fa-xl"></i></a>
+</footer>
---
+++ header.php
@@ -0,0 +1,23 @@
+<header>
+    <nav>
+        <div class="topnav" id="myTopnav">
+            <a href="index.html"><img src="images/logo.png" alt="logo"></a>
+            <a href="index.html">Accueil</a>
+            <a href="html/actualité.html">Actu équipe</a>
+            <a href="html/actualité.html">L'Inter-quartier</a>
+            <a href="html/classement_competition.html">Classement</a>
+            <a href="html/calendrier.html">Calendrier</a>
+            <a href="html/forum.html">Forum</a>
+            <a href="html/contact.html">Contact</a>
+            <a href="javascript:void(0);" class="icon" onclick="burgerMenu()">
+                <i class="fa fa-bars"></i>
+            </a>
+        </div>
+        <div class="search">
+            <input type="text" placeholder="recherchez">
+            <button type="submit">
+                <i class="fa fa-search"></i>
+            </button>
+        </div>
+    </nav>
+</header>
